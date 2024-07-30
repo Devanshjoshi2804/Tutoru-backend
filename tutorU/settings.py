a1ee142eaa3b027517c1afd4959e89e4ef8e84b2
Devanshjoshi2804 <141642062+Devanshjoshi2804@users.noreply.github.com>
2024-07-30 19:30:04 +0530

    Update settings.py

--- tutorU/settings.py
+++ tutorU/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--9+!nj^x2uj^7f5)wkbj=52(6fx%+i8+nb^y_w=f#+b^$1dmka"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'www.tutoru.in', 'tutoru.in']
 
