9af09a6c97a70ddf51c1a970b2260959b530d01c
unknown <2022.devansh.joshi@ves.ac.in>
2024-07-30 19:09:15 +0530

    Your commit message

--- tutorU/settings.py
+++ tutorU/settings.py
@@ -1,30 +1,19 @@
 from pathlib import Path
-import environ
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Base directory of the project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Initialize environment variables
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('django-insecure--9+!nj^x2uj^7f5)wkbj=52(6fx%+i8+nb^y_w=f#+b^$1dmka')
+SECRET_KEY = "django-insecure--9+!nj^x2uj^7f5)wkbj=52(6fx%+i8+nb^y_w=f#+b^$1dmka"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'www.tutoru.in', 'tutoru.in']
+
 
 # Application definition
 
@@ -61,6 +50,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://www.tutoru.in',  # Your deployed frontend domain
     'https://tutoru.in',      # Your deployed frontend domain
 ]
+
 
 ROOT_URLCONF = "tutorU.urls"
 
@@ -124,7 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
