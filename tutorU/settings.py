66bd4dd21e12d5d471db90935c0dedcfb42dd3aa
unknown <2022.devansh.joshi@ves.ac.in>
2024-07-30 23:13:12 +0530

    Your commit message

--- tutorU/settings.py
+++ tutorU/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = "django-insecure--9+!nj^x2uj^7f5)wkbj=52(6fx%+i8+nb^y_w=f#+b^$1dmka
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'www.tutoru.in', 'tutoru.in']
+ALLOWED_HOSTS = ['.vercel.app', 'www.tutoru.in', 'tutoru.in', '127.0.0.1']
 
 
 # Application definition
