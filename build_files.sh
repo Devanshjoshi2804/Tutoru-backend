d648dd16c0dd0d0739454278f8e9586b1b888d04
unknown <2022.devansh.joshi@ves.ac.in>
2024-08-03 19:48:13 +0530

    Fix build script for Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Use the virtual environment's pip
-venv/bin/python -m pip install -r new_requirements.txt
+# Install dependencies
+pip install -r new_requirements.txt
 
 # Run migrations
-venv/bin/python manage.py migrate
+python manage.py migrate
 
 # Collect static files
-venv/bin/python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
