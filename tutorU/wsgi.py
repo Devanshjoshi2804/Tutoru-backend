0f5a01051ee688506eaf85b7a78d2bcdc2578a10
Devanshjoshi2804 <141642062+Devanshjoshi2804@users.noreply.github.com>
2024-08-04 01:11:00 +0530

    Update wsgi.py

--- tutorU/wsgi.py
+++ tutorU/wsgi.py
@@ -1,11 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from django.conf import settings
-BASE_DIR = settings.BASE_DIR
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorU.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
