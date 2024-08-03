c39a6d15e4455a7cfefb0b323d2385580739718f
unknown <2022.devansh.joshi@ves.ac.in>
2024-08-03 19:40:45 +0530

    Update build script to use python3 -m pip

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-python3 -m pip install -r new_requirements.txt
-python3 manage.py collectstatic --noinput --clear
+
+# Use the virtual environment's pip
+venv/bin/python -m pip install -r new_requirements.txt
+
+# Run migrations
+venv/bin/python manage.py migrate
+
+# Collect static files
+venv/bin/python manage.py collectstatic --noinput
