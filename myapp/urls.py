ffbe305b23a9b9f2edc6813189549a8ed951f62d
Devanshjoshi2804 <141642062+Devanshjoshi2804@users.noreply.github.com>
2024-08-04 14:54:36 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import TutorRequestCreateView
 
 urlpatterns = [
-    path('api/tutor-request/', TutorRequestCreateView.as_view(), name='tutor-request'),
+    path('api/tutor-request/', TutorRequestCreateView.as_view(), name='tutor_request'),
 ]
