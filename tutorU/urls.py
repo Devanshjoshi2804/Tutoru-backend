f962b2839b824bdb009e7ef2a2c8e1ce0434244a
Devanshjoshi2804 <141642062+Devanshjoshi2804@users.noreply.github.com>
2024-08-04 11:09:56 +0530

    Update urls.py

--- tutorU/urls.py
+++ tutorU/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
+    path('myapp/api/', include('myapp.urls')),  # This should point to your app's URL configuration
 ]
