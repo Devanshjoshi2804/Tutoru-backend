9508541066c5081b730b409b2388a6c5a3a5d505
Devanshjoshi2804 <141642062+Devanshjoshi2804@users.noreply.github.com>
2024-08-04 11:48:04 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('tutor-request/', views.tutor_request, name='tutor_request'),
-    # Add other paths as needed
+    path('admin/', admin.site.urls),
+    path('myapp/', include('myapp.urls')),
 ]
