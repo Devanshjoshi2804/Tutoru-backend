6292bf617f98c74e765c06fa6d34a4a690b29cba
Devanshjoshi2804 <141642062+Devanshjoshi2804@users.noreply.github.com>
2024-08-04 10:46:41 +0530

    Update wsgi.py

--- tutorU/wsgi.py
+++ tutorU/wsgi.py
@@ -4,3 +4,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorU.settings')
 
 application = get_wsgi_application()
+
+# Define app variable for Vercel
+app = application
